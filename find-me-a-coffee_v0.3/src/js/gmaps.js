0720239003e95db368eb71b780c35f0144942419
DanTheGeekMan <danthegeekman@googlemail.com>
2016-09-14 16:43:55 +0100

    Custom marker colour and moved zoom control.

--- find-me-a-coffee_v0.3/src/js/gmaps.js
+++ find-me-a-coffee_v0.3/src/js/gmaps.js
@@ -4,6 +4,7 @@
   if (!("App" in globals)) { globals.App = {}; }
 
   globals.App.initMap = function() {
+    $('.location').on('click', this.getCurrentLocation.bind(this));
     var startingPoint = {lat: 51.5153485, lng: -0.0746975};
     let canvas  = document.getElementById('map-canvas');
 
@@ -13,6 +14,9 @@
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true,
       zoomControl: true,
+      zoomControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_TOP
+          },
       styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#597c84"},{"lightness":"-37"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":"0"},{"saturation":"0"},{"color":"#f5f5f2"},{"gamma":"1"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"-3"},{"gamma":"1.00"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#bae5ce"},{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#fac9a9"},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#787878"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.airport","elementType":"labels.icon","stylers":[{"hue":"#0a00ff"},{"saturation":"-77"},{"gamma":"0.57"},{"lightness":"0"}]},{"featureType":"transit.station.rail","elementType":"labels.text.fill","stylers":[{"color":"#43321e"}]},{"featureType":"transit.station.rail","elementType":"labels.icon","stylers":[{"hue":"#ff6c00"},{"lightness":"4"},{"gamma":"0.75"},{"saturation":"-68"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#eaf6f8"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#a8d7d8"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":"-49"},{"saturation":"-53"},{"gamma":"0.79"}]}]
     });
 
@@ -25,6 +29,23 @@
       keyword: 'coffee'
     }, this.callback.bind(this));
   };
+
+  globals.App.getCurrentLocation = function() {
+  $('#myModal').modal('hide');
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let marker = new google.maps.Marker({
+      position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+      map: globals.App.map,
+      animation: google.maps.Animation.DROP,
+      icon: {
+        url: "http://furtaev.ru/preview/user_on_map_2_small.png",
+        scaledSize: new google.maps.Size(56, 56)
+      }
+    });
+
+    globals.App.map.setCenter(marker.getPosition());
+  });
+};
 
   globals.App.callback = function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -40,8 +61,7 @@
       map: globals.App.map,
       position: place.geometry.location,
       icon: {
-        url: "http://image.flaticon.com/icons/png/128/8/8106.png",
-        // url: '../images/8106_new.png',
+        url: '../images/8106_new_new.png',
         scaledSize: new google.maps.Size(40, 40)
       }
     });
